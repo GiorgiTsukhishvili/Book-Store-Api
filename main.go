0e392102858d0fa6342e7e077af609287c8be6a1
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-11-30 15:48:28 +0400

    add gin framework and cors

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"github.com/GiorgiTsukhishvili/BookShelf-Api/config"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.New()
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	router.SetTrustedProxies([]string{"127.0.0.1"})
+
+	router.Use(config.CorsConfig())
+
+	router.Run(":3000")
+}
