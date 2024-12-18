6b80d4a8dc0c4f46899285df1305f8d667037013
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-12-18 10:40:49 +0400

    add not nutll on book columns

--- models/book.go
+++ models/book.go
@@ -7,7 +7,7 @@ type Book struct {
 	Name        string `gorm:"not null"`
 	Description string `gorm:"not null"`
 	Price       string `gorm:"not null"`
-	Image       string
-	Author      string
+	Image       string `gorm:"not null"`
+	Author      string `gorm:"not null"`
 	UserID      uint
 }
