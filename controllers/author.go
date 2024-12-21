a7aff54a0d00dfc989090f073473364980cb72f4
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-12-21 13:46:36 +0400

    add ability to fetch author

--- controllers/author.go
+++ controllers/author.go
@@ -1,8 +1,25 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func GetAuthor(ctx *gin.Context) {}
+	"github.com/GiorgiTsukhishvili/BookShelf-Api/initializers"
+	"github.com/GiorgiTsukhishvili/BookShelf-Api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func GetAuthor(ctx *gin.Context) {
+	authorID := ctx.Param("id")
+
+	var author models.Author
+
+	if err := initializers.DB.Select("id", "name", "email", "image", "type", "created_at").First(&author, "id = ?", authorID).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"author": author})
+}
 
 func GetAuthors(ctx *gin.Context) {}
 
