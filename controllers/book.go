26319e4e113d51f89a057431b20d1934098d0f7f
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-12-25 13:14:03 +0400

    add book deletion logic

--- controllers/book.go
+++ controllers/book.go
@@ -9,11 +9,11 @@ import (
 )
 
 func GetBook(ctx *gin.Context) {
-	BookID := ctx.Param("id")
+	bookID := ctx.Param("id")
 
 	var Book models.Book
 
-	if err := initializers.DB.Preload("Reviews").Preload("Genres").Preload("Author").Preload("User").First(&Book, "id = ?", BookID).Error; err != nil {
+	if err := initializers.DB.Preload("Reviews").Preload("Genres").Preload("Author").Preload("User").First(&Book, "id = ?", bookID).Error; err != nil {
 		ctx.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
 		return
 	}
@@ -34,5 +34,16 @@ func PutBook(ctx *gin.Context) {
 }
 
 func DeleteBook(ctx *gin.Context) {
+	bookId := ctx.Param("id")
 
+	if err := initializers.DB.Delete(models.Book{}, "id = ?", bookId).Error; err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Book deleted successfully",
+	})
 }
