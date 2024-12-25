07f3c12207ee983d94da142a5d1891ae36d76d4d
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-12-25 15:16:22 +0400

    add book and user on review table

--- models/review.go
+++ models/review.go
@@ -6,6 +6,8 @@ type Review struct {
 	gorm.Model
 	Rating  string `gorm:"not null"`
 	Comment string `gorm:"not null"`
+	Book    Book   `gorm:"foreignKey:BookID"`
+	User    User   `gorm:"foreignKey:UserID"`
 	UserID  uint
 	BookID  uint
 }
