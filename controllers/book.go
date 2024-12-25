02be4e4f799ae0c829959e2c9abd5ef79995445d
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-12-25 13:12:59 +0400

    add ability to fetch book

--- controllers/book.go
+++ controllers/book.go
@@ -1,11 +1,24 @@
 package controllers
 
 import (
+	"net/http"
+
+	"github.com/GiorgiTsukhishvili/BookShelf-Api/initializers"
+	"github.com/GiorgiTsukhishvili/BookShelf-Api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func GetBook(ctx *gin.Context) {
+	BookID := ctx.Param("id")
 
+	var Book models.Book
+
+	if err := initializers.DB.Preload("Reviews").Preload("Genres").Preload("Author").Preload("User").First(&Book, "id = ?", BookID).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"Book": Book})
 }
 
 func GetBooks(ctx *gin.Context) {
--- models/book.go
+++ models/book.go
@@ -10,6 +10,8 @@ type Book struct {
 	Image       string `gorm:"not null"`
 	AuthorID    uint
 	UserID      uint
+	Author      Author   `gorm:"foreignKey:AuthorID"`
+	User        User     `gorm:"foreignKey:UserID"`
 	Genres      []Genre  `gorm:"many2many:book_genres;"`
 	Reviews     []Review `gorm:"foreignKey:BookID;constraint:OnDelete:CASCADE;"`
 }
