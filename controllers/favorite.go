b1b18a403e1599134fd02705b97bd755db0e38d5
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-12-27 15:43:25 +0400

    add ability to fetch user favorite books

--- controllers/favorite.go
+++ controllers/favorite.go
@@ -1,8 +1,56 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func GetUserFavorites(ctx *gin.Context) {}
+	"github.com/GiorgiTsukhishvili/BookShelf-Api/initializers"
+	"github.com/GiorgiTsukhishvili/BookShelf-Api/models"
+	"github.com/GiorgiTsukhishvili/BookShelf-Api/requests"
+	"github.com/GiorgiTsukhishvili/BookShelf-Api/scripts"
+	"github.com/gin-gonic/gin"
+)
+
+func GetUserFavorites(ctx *gin.Context) {
+	var req requests.UserFavoriteGetRequest
+
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	claims := scripts.GetUserClaims(ctx)
+
+	paginate := scripts.Paginate(req.Page, req.Size, ctx)
+
+	var favorites []models.Favorite
+
+	if err := initializers.DB.Scopes(paginate).Preload("User").Preload("Book").Find(&favorites).Where("user_id = ?", claims.UserID).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "favorites not found"})
+		return
+	}
+
+	var totalRecords int64
+	if err := initializers.DB.Model(&models.Favorite{}).Count(&totalRecords).Error; err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	size := scripts.ConvertStringToInt(req.Size, ctx)
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": favorites,
+		"pagination": gin.H{
+			"current_page": req.Page,
+			"first_page":   1,
+			"last_page":    int(totalRecords) / size,
+			"total":        totalRecords,
+		},
+	})
+}
 
 func PostFavorite(ctx *gin.Context) {}
 
--- requests/favorite.go
+++ requests/favorite.go
@@ -1,1 +1,6 @@
 package requests
+
+type UserFavoriteGetRequest struct {
+	Page string `form:"page" binding:"required"`
+	Size string `form:"size" binding:"required"`
+}
