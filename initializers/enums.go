5458d02d7b3829e8ba5fd10a81201f80a65d7074
GiorgiTsukhishvili <gcuxishvili@gmail.com>
2024-12-28 16:05:44 +0400

    add notification type in enums

--- initializers/enums.go
+++ initializers/enums.go
@@ -8,6 +8,9 @@ func EnumsInitializer() {
 		IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_type') THEN
 			CREATE TYPE user_type AS ENUM ('user', 'business', 'admin');
 		END IF;
+		IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'notification_type') THEN
+			CREATE TYPE notification_type AS ENUM ('favorite', 'review');
+		END IF;
 	END $$;
 	`
 	err := DB.Exec(sql).Error
